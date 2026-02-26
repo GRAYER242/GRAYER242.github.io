aa550fe36ca6858fcc3d79a19239ce328eed147e
GRAYER242 <grayer.hardy@icloud.com>
2026-02-26 12:48:55 -0600

    Image Demo

---
+++ 03-images/sketch.js
@@ -0,0 +1,15 @@
+// Image Demo
+
+let luigiImg;
+function preload(){
+  luigiImg = loadImage("Luigi.png");
+}
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  imageMode(CENTER);
+}
+
+function draw() {
+  background(220);
+  image(luigiImg, mouseX, mouseY, luigiImg.width * 0.5, luigiImg.height * 0.5);
+}
--- index.md
+++ index.md
@@ -3,6 +3,7 @@
 ## Demos
 - [Moving Square](01-first-day)
 - [Scope](02-scope)
+- [Image Demo](03-images)
 
 ## Assignmnets
 - [Interactive Scene](interactive-scene)
