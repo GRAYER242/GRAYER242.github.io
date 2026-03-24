e7c11f9d0c1b3b14cb01c0c0747fda469ce43655
GRAYER242 <grayer.hardy@icloud.com>
2026-03-24 15:12:35 -0600

    Added Glider Gun to Demo

--- 09-game-of-life/sketch.js
+++ 09-game-of-life/sketch.js
@@ -1,12 +1,18 @@
-
 // 2d rectangular grid demo
 
 const CELL_SIZE = 20;
 const RENDER_ON_FRAME_MULTIPLE = 3;
+const DEAD_CELL = 0;
+const LIVE_CELL = 1;
 let autoPlayIsOn = false;
 let rows;
 let cols;
 let grid;
+let gosper;
+
+function preload() {
+  gosper = loadJSON("gosper.json");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -44,6 +50,9 @@ function keyPressed() {
   else if (key === "a") {
     autoPlayIsOn = !autoPlayIsOn;
   }
+  else if (key === "g") {
+    grid = gosper;
+  }
 }
 
 function takeTurn() {
@@ -67,23 +76,23 @@ function takeTurn() {
       neighbours -= grid[y][x];
 
       //apply the rules
-      if (grid[y][x] === 1) {
+      if (grid[y][x] === LIVE_CELL) {
         //currently alive
         if (neighbours === 2 || neighbours === 3) {
-          nextTurn[y][x] = 1;
+          nextTurn[y][x] = LIVE_CELL;
         }
         else {
-          nextTurn[y][x] = 0;
+          nextTurn[y][x] = DEAD_CELL;
         }
       }
 
-      if (grid[y][x] === 0) {
+      if (grid[y][x] === DEAD_CELL) {
         //currently dead
         if (neighbours === 3) {
-          nextTurn[y][x] = 1;
+          nextTurn[y][x] = LIVE_CELL;
         }
         else {
-          nextTurn[y][x] = 0;
+          nextTurn[y][x] = DEAD_CELL;
         }
       }
     }
@@ -94,11 +103,11 @@ function takeTurn() {
 function toggleCell(x, y) {
   //make sure the cell you're toggling is in the grid
   if (x >= 0 && x < cols && y >= 0 && y < rows) {
-    if (grid[y][x] === 0) {
-      grid[y][x] = 1;
+    if (grid[y][x] === DEAD_CELL) {
+      grid[y][x] = LIVE_CELL;
     }
-    else if (grid[y][x] === 1) {
-      grid[y][x] = 0;
+    else if (grid[y][x] === LIVE_CELL) {
+      grid[y][x] = DEAD_CELL;
     }
   }
 }
@@ -106,10 +115,10 @@ function toggleCell(x, y) {
 function displayGrid() {
   for (let y = 0; y < rows; y++) {
     for (let x = 0; x < cols; x++) {
-      if (grid[y][x] === 0) {
+      if (grid[y][x] === DEAD_CELL) {
         fill("white");
       }
-      else if (grid[y][x] === 1) {
+      else if (grid[y][x] === LIVE_CELL) {
         fill("black");
       }
       square(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE);
@@ -123,10 +132,10 @@ function generateRandomGrid(cols, rows) {
     newGrid.push([]);
     for (let x = 0; x < cols; x++) {
       if (random(100) < 50) {
-        newGrid[y].push(0);
+        newGrid[y].push(DEAD_CELL);
       }
       else {
-        newGrid[y].push(1);
+        newGrid[y].push(LIVE_CELL);
       }
     }
   }
@@ -138,7 +147,7 @@ function generateEmptyGrid(cols, rows) {
   for (let y = 0; y < rows; y++) {
     newGrid.push([]);
     for (let x = 0; x < cols; x++) {
-      newGrid[y].push(0);
+      newGrid[y].push(DEAD_CELL);
     }
   }
   return newGrid;
